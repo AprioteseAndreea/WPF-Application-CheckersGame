dc10ba87e2e1d50df454e6e3512bba0e0436f582
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-04-19 08:40:39 +0300

    Added View Model folder and Commands folder

---
+++ CheckersGame/ViewModel/CheckersGameViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CheckersGame.Model;
+using CheckersGame.Services;
+
+namespace CheckersGame.ViewModel
+{
+    public class CheckersGameViewModel
+    {
+        public static ObservableCollection<ObservableCollection<Square>> Squares { get; set; }
+        public static bool loadFromFile;
+
+        public CheckersGameViewModel()
+        {
+            if (loadFromFile == true)
+            {
+                Squares = new ObservableCollection<ObservableCollection<Square>>(ExternalHelper.InitGameBoardFromFile());
+            }
+            else
+            {
+                Squares = new ObservableCollection<ObservableCollection<Square>>(InternalHelper.InitGameBoard());
+
+            }
+
+        }
+
+
+        public static void ShowPossibleMovesForRedPiece(List<KeyValuePair<int, int>> possibleSimpleMoves)
+        {
+
+
+            foreach (KeyValuePair<int, int> pair in possibleSimpleMoves)
+            {
+                Squares[pair.Key][pair.Value].Image = "/CheckersGame;component/Resources/Pieces/RedCirclePiece.jpg";
+            }
+
+        }
+        public static void ShowPossibleMovesForWhitePiece(List<KeyValuePair<int, int>> possibleSimpleMoves)
+        {
+
+
+            foreach (KeyValuePair<int, int> pair in possibleSimpleMoves)
+            {
+                Squares[pair.Key][pair.Value].Image = "/CheckersGame;component/Resources/Pieces/WhiteCirclePiece.jpg";
+            }
+
+        }
+
+
+    }
+}
---
+++ CheckersGame/ViewModel/StatisticsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame.ViewModel
+{
+    public class StatisticsViewModel
+    {
+        public string RedWinners { get; set; }
+        public string WhiteWinners { get; set; }
+
+        public StatisticsViewModel()
+        {
+            ReadFromFile();
+        }
+        public void ReadFromFile()
+        {
+
+            String line = null;
+            try
+            {
+                StreamReader sr = new StreamReader("C:/Users/PC HOME/source/repos/CheckersGame/Resources/winners.txt");
+                line = sr.ReadLine();
+                WhiteWinners = line;
+                while (line != null)
+                {
+
+                    RedWinners = sr.ReadLine();
+                    line = null;
+
+                }
+                sr.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
