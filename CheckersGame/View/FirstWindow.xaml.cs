83a424b6e3ce4578b3d753f68beda687a226ac23
AprioteseAndreea <andreea.apriotese11@gmail.com>
2021-04-19 08:36:36 +0300

    Added View folder

---
+++ CheckersGame/View/FirstWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CheckersGame.View;
+using CheckersGame.ViewModel;
+
+namespace CheckersGame
+{
+    /// <summary>
+    /// Interaction logic for FirstWindow.xaml
+    /// </summary>
+    public partial class FirstWindow : Window
+    {
+        public FirstWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            DataContext = new CheckersGameViewModel();
+            CheckersGameViewModel.loadFromFile = false;
+
+            
+            
+        }
+
+        private void StatisticsButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new StatisticsViewModel();
+
+          
+        }
+
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new AboutViewModel();
+
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new HelpViewModel();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new CheckersGameViewModel();
+            CheckersGameViewModel.loadFromFile = true;
+
+        }
+    }
+}
---
+++ CheckersGame/View/PlayGameView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CheckersGame.Services;
+
+namespace CheckersGame.View
+{
+    /// <summary>
+    /// Interaction logic for PlayGameView.xaml
+    /// </summary>
+    public partial class PlayGameView : UserControl
+    {
+        public PlayGameView()
+        {
+            InitializeComponent();
+            ExternalHelper.ReadWinnersFromFile();
+
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RedPieceOut.Content = InternalHelper.redPieceOut.ToString();
+            WhitePieceOut.Content = InternalHelper.whitePieceOut.ToString();
+
+
+            InternalHelper.NumberOfClicks++;
+
+            verifyIsFinnish();
+            if (InternalHelper.NumberOfClicks % 2 == 0)
+            {
+                if (CurrentPlayerLabel.Content.Equals("1"))
+                {
+                    CurrentPlayerLabel.Content = "2";
+                    InternalHelper.CurrentPlayer = 2;
+                }
+                else
+                {
+                    CurrentPlayerLabel.Content = "1";
+                    InternalHelper.CurrentPlayer = 1;
+                }
+
+            }
+        }
+        public static void verifyIsFinnish()
+        {
+            if (InternalHelper.redPieceOut == 12)
+            {
+                MessageBox.Show(" Player 1 is the winner!");
+                InternalHelper.WhiteWinners++;
+            }
+            if (InternalHelper.whitePieceOut == 12)
+            {
+                MessageBox.Show(" Player 2 is the winner!");
+                InternalHelper.RedWinners++;
+            }
+            ExternalHelper.WriteWinnersToFile();
+        }
+
+        private void SaveGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            ExternalHelper.SaveGameInFile();
+        }
+    }
+}
